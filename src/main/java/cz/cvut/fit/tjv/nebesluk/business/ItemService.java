a2a80d1a5d50f22c8038c1b09d24bacfcab9b3d8
Lukáš Nebeský <nebesluk@fit.cvut.cz>
2022-10-27 13:20:48 +0200

    Add base for business

--- src/main/java/cz/cvut/fit/tjv/nebesluk/business/ItemService.java
+++ src/main/java/cz/cvut/fit/tjv/nebesluk/business/ItemService.java
@@ -4,7 +4,11 @@ import cz.cvut.fit.tjv.nebesluk.dao.repository.ItemRepository;
 import cz.cvut.fit.tjv.nebesluk.domain.Item;
 
 public class ItemService extends CrudService<Item,Long>{
-    public ItemService(ItemRepository itemRepository){
+
+    protected ClientService clientService;
+
+    public ItemService(ItemRepository itemRepository,ClientService _clientService){
         super(itemRepository);
+        clientService = _clientService;
     }
 }
