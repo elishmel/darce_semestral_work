9f1a71ba3c632a101228a565f790949a04b303f3
Lukáš Nebeský <nebesluk@fit.cvut.cz>
2022-12-27 21:39:18 +0100

    Add query for receivers

--- src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/repository/ClientRepository.java
+++ src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/repository/ClientRepository.java
@@ -1,6 +1,7 @@
 package cz.cvut.fit.nebesluk.semestral.nebesluk_darce.repository;
 
 import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.domain.Client;
+import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
