eeef45767eab9a5ba3323cc836375d283606bf65
Lukáš Nebeský <nebesluk@fit.cvut.cz>
2022-12-27 21:50:00 +0100

    Add missing @Repository tags

--- src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/repository/ImageRepository.java
+++ src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/repository/ImageRepository.java
@@ -2,7 +2,9 @@ package cz.cvut.fit.nebesluk.semestral.nebesluk_darce.repository;
 
 import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.domain.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image,Long> {
 
 }
