bea684ed46e98682906b79cf8c157395efe39772
Lukáš Nebeský <nebesluk@fit.cvut.cz>
2022-12-27 21:47:45 +0100

    Add @Service tags

--- src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/service/ItemService.java
+++ src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/service/ItemService.java
@@ -2,7 +2,9 @@ package cz.cvut.fit.nebesluk.semestral.nebesluk_darce.service;
 
 import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.domain.Item;
 import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.repository.ItemRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ItemService extends AbstractService<Item,Long>{
     public ItemService(ItemRepository repository){
         super(repository);
--- src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/service/TagService.java
+++ src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/service/TagService.java
@@ -2,7 +2,9 @@ package cz.cvut.fit.nebesluk.semestral.nebesluk_darce.service;
 
 import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.domain.Tag;
 import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.repository.TagRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TagService extends AbstractService<Tag,String> {
 
     public TagService(TagRepository repository){
