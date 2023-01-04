189c458b7242251d4eb986d7b5787c82f70a725d
Lukáš Nebeský <nebesluk@fit.cvut.cz>
2023-01-04 23:20:39 +0100

    Add Tag api CUD (R) already present

--- src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/api/TagController.java
+++ src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/api/TagController.java
@@ -1,10 +1,13 @@
 package cz.cvut.fit.nebesluk.semestral.nebesluk_darce.api;
 
 import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.domain.Tag;
+import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.exceptions.EntityAlreadyExistsException;
+import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.exceptions.EntityNotExistsException;
+import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.exceptions.EntityStateException;
 import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.service.TagService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 
@@ -21,5 +24,32 @@ public class TagController {
     @GetMapping
     public Collection<Tag> GetAll(){
         return tagService.ReadAll();
+    }
+
+    @PostMapping
+    public Tag Create(@RequestBody Tag tag){
+        try{
+            return tagService.Create(tag);
+        } catch (EntityAlreadyExistsException e){
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public Tag Update(@RequestBody Tag tag, @PathVariable String id){
+        try{
+            return tagService.Update(tag);
+        } catch (EntityNotExistsException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void Delete(@PathVariable String id){
+        try{
+            tagService.DeleteById(id);
+        } catch (EntityNotExistsException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
