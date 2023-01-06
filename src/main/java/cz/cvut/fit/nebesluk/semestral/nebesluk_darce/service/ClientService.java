eb616dd0b8d83f1c76b3875c8360465be11f5ec0
Lukáš Nebeský <nebesluk@fit.cvut.cz>
2023-01-06 05:49:41 +0100

    Add Put and delete to client

--- src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/service/ClientService.java
+++ src/main/java/cz/cvut/fit/nebesluk/semestral/nebesluk_darce/service/ClientService.java
@@ -3,6 +3,7 @@ package cz.cvut.fit.nebesluk.semestral.nebesluk_darce.service;
 import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.domain.Client;
 import cz.cvut.fit.nebesluk.semestral.nebesluk_darce.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,19 @@ public class ClientService extends AbstractService<Client,Long> {
                 roles("USER").
                 build());
         return this.Create(client);
+    }
+
+    public void UpdateCredentials(String oldUsername,String username, String password){
+        var roles = userDetailsManager.loadUserByUsername(oldUsername).getAuthorities();
+        userDetailsManager.deleteUser(oldUsername);
+        userDetailsManager.createUser(User.builder().username(username).password(
+                PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(password))
+                .authorities(roles).build());
+    }
+
+    public void DeleteCredentials(Long id){
+        var user = this.ReadById(id);
+        userDetailsManager.deleteUser(user.get().getUsername());
     }
 
     public Optional<Client> ReadByUsername(String username){
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO tags (tag) VALUES ('Jídlo') ON CONFLICT DO NOTHING;
 INSERT INTO tags (tag) VALUES ('Zdravotní') ON CONFLICT DO NOTHING;
 INSERT INTO tags (tag) VALUES ('Koníčky') ON CONFLICT DO NOTHING;
 
-INSERT INTO images (image_id, url) VALUES (0,'http://localhost:8080/images/Basic_human_drawing.png') ON CONFLICT DO NOTHING;
+INSERT INTO images (image_id, url) VALUES (0,'http://localhost:8080/pictures/Basic_human_drawing.png') ON CONFLICT DO NOTHING;
 
 INSERT INTO clients (client_id, date_created, date_last_logon, real_name, username, profile_picture_image_id) VALUES
-                                                                                                                  (0,now(),now(),'Testovací uživatel','Lumír',0);
+                                                                                                                  (0,now(),now(),'Testovací uživatel','Lumir',0);
